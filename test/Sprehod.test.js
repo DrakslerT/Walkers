7e29f5fc7624a250019766caba52ad46d77ced11
iletavcioski <it8816@student.uni-lj.si>
2021-05-23 10:47:42 +0200

    zbirsni komentarji

--- test/Sprehod.test.js
+++ test/Sprehod.test.js
@@ -15,4 +15,47 @@ const {
           email: "ilija.tavcioski@gmail.com",
           password: "123456789",
       };
+
+      it(`Login ile`,(done) => {
+        supertestRequest(app)
+        .post('/api/login')
+        .send(userile)
+        .expect(200)
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+                return done(err);
+            }
+            token = res.body.accessToken;
+            userile = { ...userile, token };
+            return done();
+        });
+    });
+      it(`Get walks`,(done) => {
+          supertestRequest(app)
+          .post('/api/walkNotifications')
+          .set("Authorization", `Bearer ${userile.token}`)
+          .expect(200)
+          .end(function(err, res) {
+              if (err) {
+                  console.error(err);
+                  return done(err);
+              }
+              return done();
+          });
+      });
+
+      it(`Get oglasi`,(done) => {
+        supertestRequest(app)
+        .get('/api/oglas/me')
+        .set("Authorization", `Bearer ${userile.token}`)
+        .expect(200)
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+                return done(err);
+            }
+            return done();
+        });
+    });
   });
